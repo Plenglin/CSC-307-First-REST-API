2097b82f1a85fe810973aecf905980b57e64ea91
Astrid Yu <astrid@astrid.tech>
2021-01-12 16:58:43 -0800

    Add in provided template code

--- main.py
+++ main.py
@@ -1,37 +1,60 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-users = { 
-   'users_list' :
-   [
-      { 
-         'id' : 'xyz789',
-         'name' : 'Charlie',
-         'job': 'Janitor',
-      },
-      {
-         'id' : 'abc123', 
-         'name': 'Mac',
-         'job': 'Bouncer',
-      },
-      {
-         'id' : 'ppp222', 
-         'name': 'Mac',
-         'job': 'Professor',
-      }, 
-      {
-         'id' : 'yat999', 
-         'name': 'Dee',
-         'job': 'Aspring actress',
-      },
-      {
-         'id' : 'zap555', 
-         'name': 'Dennis',
-         'job': 'Bartender',
-      }
-   ]
+users = {
+    'users_list':
+    [
+        {
+            'id': 'xyz789',
+            'name': 'Charlie',
+            'job': 'Janitor',
+        },
+        {
+            'id': 'abc123',
+            'name': 'Mac',
+            'job': 'Bouncer',
+        },
+        {
+            'id': 'ppp222',
+            'name': 'Mac',
+            'job': 'Professor',
+        },
+        {
+            'id': 'yat999',
+            'name': 'Dee',
+            'job': 'Aspring actress',
+        },
+        {
+            'id': 'zap555',
+            'name': 'Dennis',
+            'job': 'Bartender',
+        }
+    ]
 }
+
+
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+    search_username = request.args.get('name')
+    if request.method == 'GET':
+        if search_username:
+            subdict = {'users_list': []}
+            for user in users['users_list']:
+                if user['name'] == search_username:
+                    subdict['users_list'].append(user)
+            return subdict
+        return users
+    elif request.method == 'POST':
+        userToAdd = request.get_json()
+        users['users_list'].append(userToAdd)
+        resp = jsonify(success=True)
+        # resp.status_code = 200 #optionally, you can always set a response code.
+        # 200 is the default code for a normal response
+        return resp
+    raise Exception("Unsupported method")
+
 
 @app.route('/')
 def hello_world():
-	return 'Hello, world!'
+    return 'Hello, world!'
